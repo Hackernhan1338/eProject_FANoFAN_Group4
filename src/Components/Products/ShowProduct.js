ab36ccecb44d4410f40333ee2f4fceabf66622ce
unknown <trantramella@gmail.com>
2023-10-11 08:57:43 +0700

    reinitialized files

--- src/Components/Products/ShowProduct.js
+++ src/Components/Products/ShowProduct.js
@@ -12,6 +12,8 @@ function ShowProduct({ Add, ProductCart }) {
   let Detail = Product.filter((x) => x.ID == id);
   Detail = Detail[0];
 
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+
   return (
     <div>
       <Container fluid="md">
